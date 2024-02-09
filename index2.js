ecb9704320155dbe32473c457fc34f341c923afd
ju3tin <ju3tin@hotmail.co.uk>
2024-02-09 02:21:57 +0000

    sdf

---
+++ index2.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+const fs = require('fs');
+const axios = require('axios');
+
+const crack = 'sd';
+
+const app = express();
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true,
+    limits: { 
+        fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
+    },
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+// upoad single file
+app.post('/upload-avatar', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+            const crack = avatar.name;
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            avatar.mv('./tmp/' + avatar.name).then(
+               // console.log('crack is cool')
+                () => {
+                    const axios = require('axios');
+let data5 = './tmp/'+avatar.name;
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://api.wit.ai/speech',
+  headers: { 
+    'Content-Type': 'audio/wav', 
+    'Authorization': 'Bearer WN27BV76PBRPKC3MLZIWFYRJPEZEFXEJ'
+  },
+  data : data5
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+ 
+})
+.catch((error) => {
+  console.log(error);
+});
+
+                  },
+            )
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                dude: response.data,
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+            
+
+            //send response
+          
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
+
+app.get('/dude.json', (req, res) => {
+ 
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://api.printify.com/v1/shops/14136938/products.json',
+      headers: { 
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzN2Q0YmQzMDM1ZmUxMWU5YTgwM2FiN2VlYjNjY2M5NyIsImp0aSI6ImVjODRkYTZjMWEyNjRkNThiZjgwNTNkZDJiOTY0OTJjZWE2ZjViY2RlYWE5M2Q4YTY4NDMwYzZhOTRiYmQ5Y2NkZWE3MGIzM2M1MzIxOGQ4IiwiaWF0IjoxNzA2NzA2Mzk1LjcwNTk3NCwibmJmIjoxNzA2NzA2Mzk1LjcwNTk3NiwiZXhwIjoxNzM4MzI4Nzk1LjY5OTUyLCJzdWIiOiIxMzg4OTc3NSIsInNjb3BlcyI6WyJzaG9wcy5tYW5hZ2UiLCJzaG9wcy5yZWFkIiwiY2F0YWxvZy5yZWFkIiwib3JkZXJzLnJlYWQiLCJvcmRlcnMud3JpdGUiLCJwcm9kdWN0cy5yZWFkIiwicHJvZHVjdHMud3JpdGUiLCJ3ZWJob29rcy5yZWFkIiwid2ViaG9va3Mud3JpdGUiLCJ1cGxvYWRzLnJlYWQiLCJ1cGxvYWRzLndyaXRlIiwicHJpbnRfcHJvdmlkZXJzLnJlYWQiXX0.Aj0cBLyGVuvg5f8-FcJ2QtY3dncxL8kBiCuEu6wCXOExkcgFlOB_Nu_HB9qj3awCLuM4IHyLOC6jbfAunF0'    }
+    };
+    
+    axios.request(config)
+    .then((response) => {
+    //  console.log(JSON.stringify(response.data));
+    //  res.writeHead(200, { 'Content-Type': 'text/html' });
+  
+      // Set response content    
+      res.write(JSON.stringify(response.data));
+    //  res.status(200).json('Welcome, your app is working well');
+      res.send();//end the response
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
+  })
+
+//make uploads directory static
+app.use(express.static('tmp'));
+
+//start app 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => 
+  console.log(`App is listening on port ${port}.`)
+);

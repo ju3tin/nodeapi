bbfc8623d90bc7b9d88506737b585741ab9613dc
Justin <ju3tin@hotmail.co.uk>
2024-06-15 16:37:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -501,12 +501,12 @@ app.get('/dude.json', (req, res) => {
 })
 
 
-app.get('/dude5.json', (req, res) => {
- 
+app.get('/dude5', (req, res) => {
+  const { skip = 0 } = req.query;
   let config = {
     method: 'get',
     maxBodyLength: Infinity,
-    url: 'https://members-api.parliament.uk/api/Location/Constituency/Search?skip=1&take=20',
+    url: `https://members-api.parliament.uk/api/Location/Constituency/Search?skip=${skip}&take=20`,
     headers: { 
       'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzN2Q0YmQzMDM1ZmUxMWU5YTgwM2FiN2VlYjNjY2M5NyIsImp0aSI6ImVjODRkYTZjMWEyNjRkNThiZjgwNTNkZDJiOTY0OTJjZWE2ZjViY2RlYWE5M2Q4YTY4NDMwYzZhOTRiYmQ5Y2NkZWE3MGIzM2M1MzIxOGQ4IiwiaWF0IjoxNzA2NzA2Mzk1LjcwNTk3NCwibmJmIjoxNzA2NzA2Mzk1LjcwNTk3NiwiZXhwIjoxNzM4MzI4Nzk1LjY5OTUyLCJzdWIiOiIxMzg4OTc3NSIsInNjb3BlcyI6WyJzaG9wcy5tYW5hZ2UiLCJzaG9wcy5yZWFkIiwiY2F0YWxvZy5yZWFkIiwib3JkZXJzLnJlYWQiLCJvcmRlcnMud3JpdGUiLCJwcm9kdWN0cy5yZWFkIiwicHJvZHVjdHMud3JpdGUiLCJ3ZWJob29rcy5yZWFkIiwid2ViaG9va3Mud3JpdGUiLCJ1cGxvYWRzLnJlYWQiLCJ1cGxvYWRzLndyaXRlIiwicHJpbnRfcHJvdmlkZXJzLnJlYWQiXX0.Aj0cBLyGVuvg5f8-FcJ2QtY3dncxL8kBiCuEu6wCXOExkcgFlOB_Nu_HB9qj3awCLuM4IHyLOC6jbfAunF0'    }
   };
